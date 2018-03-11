e413cfe4e0238999248a85f241e140fce6f455f5
Tom Spilman <tom@sickheadgames.com>
2018-03-10 22:50:45 -0600

    Fixed unit tests to be EXE. Expanded simple test into longer stress test to verify stable native interop.

--- project/vc12/Nvidia.TextureTools.UnitTests/Test.cs
+++ project/vc12/Nvidia.TextureTools.UnitTests/Test.cs
@@ -1,66 +1,147 @@
-﻿using NUnit.Framework;
-using System;
-using Nvidia.TextureTools;
+﻿using System;
 using System.Runtime.InteropServices;
+using NUnitLite;
+using NUnit.Framework;
 
-namespace Nvidia.TextureTools.UnitTests {
-	[TestFixture ()]
-	public class Test {
-		[Test ()]
-		public void TestCase ()
-		{
-			var inputOptions = new InputOptions ();
-			var outputOptions = new OutputOptions ();
-			var compressionOptions = new CompressionOptions ();
-			var compressor = new Compressor ();
-			inputOptions.SetAlphaMode (AlphaMode.Premultiplied);
-			inputOptions.SetTextureLayout (TextureType.Texture2D, 128, 128, 1);
-			byte [] sourceData = new byte [128*128*4];
-			var dataHandle = GCHandle.Alloc (sourceData, GCHandleType.Pinned);
-			var BeginImage = new OutputOptions.BeginImageHandler (BeginImageInternal);
-			var WriteData = new OutputOptions.OutputHandler (WriteDataInternal);
-			var EndImage = new OutputOptions.EndImageHandler (EndImageInternal);
-			var a = GCHandle.Alloc (BeginImage);
-			var b = GCHandle.Alloc (WriteData);
-			var c = GCHandle.Alloc (EndImage);
-			try {
-				var dataPtr = dataHandle.AddrOfPinnedObject ();
-				inputOptions.SetMipmapData (dataPtr, 128, 128, 1, 0, 0);
-				inputOptions.SetMipmapGeneration (false);
-				inputOptions.SetGamma (1.0f, 1.0f);
-				compressionOptions.SetFormat (Format.RGBA);
-				outputOptions.SetOutputHeader (false);
-				outputOptions.SetOutputOptionsOutputHandler (BeginImage, WriteData, EndImage);
-				var estsize = compressor.EstimateSize (inputOptions, compressionOptions);
-				Assert.True (compressor.Compress (inputOptions, compressionOptions, outputOptions));
-				Assert.AreEqual (estsize, buffer.Length);
-			}finally {
-				a.Free ();
-				b.Free ();
-				c.Free ();
-				dataHandle.Free ();
-			}
-		}
+namespace Nvidia.TextureTools.UnitTests 
+{
+    [TestFixture]
+    public class Test
+    {
+        private static int _count;
 
-		byte [] buffer;
-		int offset;
+        [Test]
+        [Repeat(100)]
+        [Parallelizable]
+        public void StressTest()
+        {
+            var count = ++_count;
+            var seed = unchecked(123456 * count);
+            Console.WriteLine("StressTest Seed=" + seed);
 
-		void BeginImageInternal (int size, int width, int height, int depth, int face, int miplevel)
-		{
-			buffer = new byte [size];
-			offset = 0;
-		}
+            TestRandomAllFormats(32, 32, seed);
+            TestRandomAllFormats(32, 64, seed+1);
+            TestRandomAllFormats(61, 127, seed+2);
+            TestRandomAllFormats(1024, 32, seed+3);
+            TestRandomAllFormats(32, 1024, seed+4);
+            TestRandomAllFormats(1024, 1024, seed+5);
+        }
 
-		bool WriteDataInternal (IntPtr data, int length)
-		{
-			Marshal.Copy (data, buffer, offset, length);
-			offset += length;
-			return true;
-		}
+        private void TestRandomAllFormats(int sizeX, int sizeY, int seed)
+        {
+            TestRandom(sizeX, sizeY, seed, Format.DXT1, AlphaMode.Premultiplied, Quality.Highest);
+            TestRandom(sizeX, sizeY, seed+1, Format.DXT1a, AlphaMode.Premultiplied, Quality.Highest);
+            TestRandom(sizeX, sizeY, seed+2, Format.DXT3, AlphaMode.Premultiplied, Quality.Highest);
+            TestRandom(sizeX, sizeY, seed+3, Format.DXT5, AlphaMode.Premultiplied, Quality.Highest);
+        }
 
-		void EndImageInternal ()
-		{
-			// add done write the buffer.
-		}
-	}
+        private void TestRandom(int sizeX, int sizeY, int seed, Format format, AlphaMode alphaMode, Quality quality)
+        {
+            var data = CreateRandomTexture(sizeX, sizeY, seed);
+            TestCompress(sizeX, sizeY, data, Format.DXT1, AlphaMode.Premultiplied, Quality.Highest);
+        }
+
+        private static byte[] CreateRandomTexture(int sizeX, int sizeY, int seed)
+        {
+            var rand = new Random(seed);
+
+            var data = new byte[sizeX * sizeY * 4];
+            for (int i = 0; i < data.Length; i += 4)
+            {
+                data[i + 0] = (byte)rand.Next(0, 255);
+                data[i + 1] = (byte)rand.Next(0, 255);
+                data[i + 2] = (byte)rand.Next(0, 255);
+                data[i + 3] = 255;
+            }
+
+            return data;
+        }
+
+        private void TestCompress(int sizeX, int sizeY, byte[] data, Format format, AlphaMode alphaMode, Quality quality)
+        {
+            var dataHandle = GCHandle.Alloc(data, GCHandleType.Pinned);
+
+            try
+            {
+                var dataPtr = dataHandle.AddrOfPinnedObject();
+
+                var inputOptions = new InputOptions();
+                inputOptions.SetTextureLayout(TextureType.Texture2D, sizeX, sizeY, 1);
+                inputOptions.SetMipmapData(dataPtr, sizeX, sizeY, 1, 0, 0);
+                inputOptions.SetMipmapGeneration(false);
+                inputOptions.SetGamma(1.0f, 1.0f);
+                inputOptions.SetAlphaMode(alphaMode);
+
+                var compressionOptions = new CompressionOptions();
+                compressionOptions.SetFormat(format);
+                compressionOptions.SetQuality(quality);
+                compressionOptions.SetPixelFormat(32, 0x000000FF, 0x0000FF00, 0x00FF0000, 0xFF000000);
+
+                var dataHelper = new DataHelper();
+
+                var outputOptions = new OutputOptions();
+                outputOptions.SetOutputHeader(false);
+                outputOptions.SetOutputOptionsOutputHandler(dataHelper.BeginImageInternal, dataHelper.WriteDataInternal, dataHelper.EndImageInternal);
+                outputOptions.Error += OutputOptionsOnError;
+
+                Collect();
+
+                var compressor = new Compressor();
+                var estsize = compressor.EstimateSize(inputOptions, compressionOptions);
+                Assert.True(compressor.Compress(inputOptions, compressionOptions, outputOptions));
+
+                Collect();
+
+                Assert.AreEqual(estsize, dataHelper.buffer.Length);                
+            }
+            finally
+            {
+                dataHandle.Free();
+            }
+        }
+
+        private void OutputOptionsOnError(Error error)
+        {
+            Console.WriteLine("OutputOptionsOnError! " + error);
+            throw new Exception();
+        }
+
+        private static void Collect()
+        {
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+            GC.Collect(); 
+        }
+
+        class DataHelper
+        {
+            public byte[] buffer;
+            int offset;
+
+            public void BeginImageInternal(int size, int width, int height, int depth, int face, int miplevel)
+            {
+                Collect();
+                buffer = new byte [size];
+                offset = 0;
+            }
+
+            public bool WriteDataInternal(IntPtr data, int length)
+            {
+                Collect();
+                Marshal.Copy(data, buffer, offset, length);
+                offset += length;
+                return true;
+            }
+
+            public void EndImageInternal()
+            {
+                Collect();
+            }
+        }
+
+        public static int Main(String[] args)
+        {
+            return new AutoRun().Execute(args);
+        }        
+    }    
 }
